cb8ccb5da34d2ede59918e658fb3b7600d33140b
sanghabrands <franco@sanghabrands.com>
2017-10-05 10:46:30 +0100

    General tweaks

--- assets/fonts/CerebriBold.js
+++ assets/fonts/CerebriBold.js
@@ -1,6 +1,6 @@
 /*
 
- MyFonts Webfont Build ID 3458889, 2017-10-01T18:24:50-0400
+ MyFonts Webfont Build ID 3458890, 2017-10-01T18:26:14-0400
 
  The fonts listed in this notice are subject to the End User License
  Agreement(s) entered into by the website owner. All other parties are 
@@ -8,20 +8,17 @@
 
  You may obtain a valid license at the URLs below.
 
- Webfont: CerebriSans-Light by Hanken Design Co.
- URL: https://www.myfonts.com/fonts/hanken-designco/cerebri-sans/light/
-
- Webfont: CerebriSans-Medium by Hanken Design Co.
- URL: https://www.myfonts.com/fonts/hanken-designco/cerebri-sans/medium/
-
-
- License: https://www.myfonts.com/viewlicense?type=web&buildid=3458889
+ Webfont: CerebriSans-Bold by Hanken Design Co.
+ URL: https://www.myfonts.com/fonts/hanken-designco/cerebri-sans/bold/
+ Copyright: Copyright &#x00A9;2016. Cerebri Sans is a trademark of Alfredo Marco Pradil. All rights reserved.
  Licensed pageviews: 20,000
- Webfonts copyright: Copyright &#x00A9;2016. Cerebri Sans is a trademark of Alfredo Marco Pradil. All rights reserved.
+
+
+ License: https://www.myfonts.com/viewlicense?type=web&buildid=3458890
 
  ? 2017 MyFonts Inc
 */
-var protocol=document.location.protocol;"https:"!=protocol&&(protocol="http:");var count=document.createElement("script");count.type="text/javascript";count.async=!0;count.src=protocol+"//hello.myfonts.net/count/34c749";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(count,s);var browserName,browserVersion,webfontType;if("undefined"==typeof woffEnabled)var woffEnabled=!0;var svgEnabled=0,woff2Enabled=1;
+var protocol=document.location.protocol;"https:"!=protocol&&(protocol="http:");var count=document.createElement("script");count.type="text/javascript";count.async=!0;count.src=protocol+"//hello.myfonts.net/count/34c74a";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(count,s);var browserName,browserVersion,webfontType;if("undefined"==typeof woffEnabled)var woffEnabled=!0;var svgEnabled=0,woff2Enabled=1;
 if("undefined"!=typeof customPath)var path=customPath;else{var scripts=document.getElementsByTagName("SCRIPT"),script=scripts[scripts.length-1].src;script.match("://")||"/"==script.charAt(0)||(script="./"+script);path=script.replace(/\\/g,"/").replace(/\/[^\/]*\/?$/,"")}
 var wfpath=path+"/webfonts/",browsers=[{regex:"MSIE (\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:9,type:"woff"},{version:5,type:"eot"}]},{regex:"Trident/(\\d+\\.\\d+); (.+)?rv:(\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$3)",type:[{version:11,type:"woff"}]},{regex:"Firefox[/s](\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:3.6,type:"woff"},{version:3.5,type:"ttf"}]},{regex:"Edge/(\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:12,type:"woff"}]},
 {regex:"Chrome/(\\d+\\.\\d+)",versionRegex:"new Number(RegExp.$1)",type:[{version:36,type:"woff2"},{version:6,type:"woff"},{version:4,type:"ttf"}]},{regex:"Mozilla.*Android (\\d+\\.\\d+).*AppleWebKit.*Safari",versionRegex:"new Number(RegExp.$1)",type:[{version:4.1,type:"woff"},{version:3.1,type:"svg#wf"},{version:2.2,type:"ttf"}]},{regex:"Mozilla.*(iPhone|iPad).* OS (\\d+)_(\\d+).* AppleWebKit.*Safari",versionRegex:"new Number(RegExp.$2) + (new Number(RegExp.$3) / 10)",unhinted:!0,type:[{version:5,
@@ -29,5 +26,5 @@ type:"woff"},{version:4.2,type:"ttf"},{version:1,type:"svg#wf"}]},{regex:"Mozill
 {version:10.1,type:"ttf"}]}],browLen=browsers.length,suffix="",i=0;
 a:for(;i<browLen;i++){var regex=new RegExp(browsers[i].regex);if(regex.test(navigator.userAgent)){browserVersion=eval(browsers[i].versionRegex);var typeLen=browsers[i].type.length;for(j=0;j<typeLen;j++)if(browserVersion>=browsers[i].type[j].version&&(1==browsers[i].unhinted&&(suffix="_unhinted"),webfontType=browsers[i].type[j].type,"woff"!=webfontType||woffEnabled)&&("woff2"!=webfontType||woff2Enabled)&&("svg#wf"!=webfontType||svgEnabled))break a}else webfontType="woff"}
 /(Macintosh|Android)/.test(navigator.userAgent)&&"svg#wf"!=webfontType&&(suffix="_unhinted");var head=document.getElementsByTagName("head")[0],stylesheet=document.createElement("style");stylesheet.setAttribute("type","text/css");head.appendChild(stylesheet);
-for(var fonts=[{fontFamily:"CerebriSans-Light",url:wfpath+"34C749_0"+suffix+"_0."+webfontType},{fontFamily:"CerebriSans-Medium",url:wfpath+"34C749_1"+suffix+"_0."+webfontType}],len=fonts.length,css="",i=0;i<len;i++){var format="svg#wf"==webfontType?'format("svg")':"ttf"==webfontType?'format("truetype")':"eot"==webfontType?"":'format("'+webfontType+'")',css=css+("@font-face{font-family: "+fonts[i].fontFamily+";src:url("+fonts[i].url+")"+format+";");fonts[i].fontWeight&&(css+="font-weight: "+fonts[i].fontWeight+
-";");fonts[i].fontStyle&&(css+="font-style: "+fonts[i].fontStyle+";");css+="}"}stylesheet.styleSheet?stylesheet.styleSheet.cssText=css:stylesheet.innerHTML=css;
+for(var fonts=[{fontFamily:"CerebriSans-Bold",url:wfpath+"34C74A_0"+suffix+"_0."+webfontType}],len=fonts.length,css="",i=0;i<len;i++){var format="svg#wf"==webfontType?'format("svg")':"ttf"==webfontType?'format("truetype")':"eot"==webfontType?"":'format("'+webfontType+'")',css=css+("@font-face{font-family: "+fonts[i].fontFamily+";src:url("+fonts[i].url+")"+format+";");fonts[i].fontWeight&&(css+="font-weight: "+fonts[i].fontWeight+";");fonts[i].fontStyle&&(css+="font-style: "+fonts[i].fontStyle+";");
+css+="}"}stylesheet.styleSheet?stylesheet.styleSheet.cssText=css:stylesheet.innerHTML=css;
